436711bb8feb7e0e73fa3fbd1123187df3a5f2e0
Stephen Thomas-Dorin <stephen.thomasdorin@gmail.com>
2022-10-05 23:31:10 -0700

    Render squares in VirtualizedList (ScrollView)

--- App.js
+++ App.js
@@ -1,20 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, SafeAreaView, View, VirtualizedList } from 'react-native';
+
+const map = new Map();
+
+const generateColor = () => {
+  const randomColor = Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0');
+  return `#${randomColor}`;
+};
+
+const getItemCount = (data) => 500;
+
+const getItem = (data, index) => {
+  if (map.has(index)) {
+    return map.get(index);
+  }
+
+  let item = {
+    key: Math.random().toString(12).substring(0),
+    color: generateColor(),
+  };
+  map.set(index, item);
+  return item;
+}
+
+const renderPost = ({ item }) => {
+  return (
+    <View style={styles.post}>
+      <Text style={styles.userName}>{item.color}</Text>
+      <View style={[styles.square, {backgroundColor: item.color}]} />
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.header}>Scroll</Text>
+      <VirtualizedList
+        style={styles.scrollView}
+        data={[]}
+        initialNumToRender={4}
+        renderItem={renderPost}
+        getItemCount={getItemCount}
+        getItem={getItem} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#000',
+  },
+  header: {
+    color: "#fff",
+    fontSize: 32,
+    fontWeight: "bold",
+    padding: 6,
+  },
+  scrollView: {
+    borderTopWidth: 1,
+    borderTopColor: "#333",
+  },
+  post: {
+    marginBottom: 6,
+  },
+  userName: {
+    color: "#fff",
+    fontWeight: "bold",
+    padding: 12,
+  },
+  square: {
+    flex: 1,
+    height: 400,
   },
 });
